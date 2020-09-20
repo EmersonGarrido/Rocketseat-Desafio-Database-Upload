09f6edbe18e465d67c12ef32eaee3fd943a0cbb6
Emerson Garrido <emersongarrido@hotmail.com.br>
2020-09-20 00:15:26 -0400

    chore: finish upload file csv  🚀

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  Entity,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
